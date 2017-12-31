e3a39ef14b86906778a85bd26d0f801da42f9b3d
Simone <simone@MacBook-Pro-di-Simone.local>
2017-12-31 18:44:23 +0100

    first commit

---
+++ R/RGMQLScalaLibs.R
@@ -0,0 +1,13 @@
+#' RGMQLScalaLibs
+#'
+#' Is a data package containing two compiled libraries in jar format used to
+#' invoke GMQL scala API
+#'
+#' This library is used in the package RGQML
+#' @name RGMQLScalaLibs
+#' @rdname RGMQLScalaLibs
+#' @title RGMQL scala API libraries
+#' @docType package
+#' @aliases RGMQLScalaLibs-package
+#'
+NULL
---
+++ README.md
@@ -0,0 +1,23 @@
+# RGMQL
+
+This package works with java 1.8
+
+## Settings OSX
+
+edit .bash_profile, add environment variable
+
+export JAVA_HOME="<java_path>"
+
+export PATH=$PATH
+
+edit /etc/paths add these
+
+$JAVA_HOME/bin
+
+
+## Settings Windows
+
+create environment variable JAVA_HOME 
+
+
+
