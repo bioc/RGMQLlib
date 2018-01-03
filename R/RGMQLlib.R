4dba6c2bc3bfe24bef701bd20465a7f407f5687a
Simone <simone@MacBook-Pro-di-Simone.local>
2018-01-03 12:25:17 +0100

    change name

--- R/RGMQLlib.R
+++ R/RGMQLlib.R
@@ -1,13 +1,13 @@
-#' RGMQLScalaLib
+#' RGMQLlib
 #'
 #' Is a data package containing compiled libraries in jar format used to
 #' invoke GMQL scala API
 #'
 #' This library is used in the package RGQML
-#' @name RGMQLScalaLib
-#' @rdname RGMQLScalaLib
+#' @name RGMQLlib
+#' @rdname RGMQLlib
 #' @title RGMQL scala API libraries
 #' @docType package
-#' @aliases RGMQLScalaLib-package
+#' @aliases RGMQLlib-package
 #'
 NULL
